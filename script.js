8356688e3cfab3cbd6445f9e2e48ce14c8428f63
Yi Fong Au <yifongau@gmail.com>
2024-12-24 23:02:35 +0100

    refactor

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
 const chordEl = document.getElementById('chord')
 const chordBtn = document.getElementById('chordBtn')
+const chrdParams = new Object(); // possible chord parameters
+    chrdParams.letters = ['C','D','E','F','G','A','B']
+    chrdParams.accidentals = ['','♭','♯']
+    chrdParams.qualities = ['Δ7','7','m7','ø']
+    chrdParams.bassNotes = ['','/III','/V','/VII']
 
-chordBtn.addEventListener('click', generateChord)
-generateChord()
+let chordCache = generateChords(chrdParams) // possible chords
+const printChord = function() {  
+  print(random(chordCache))
+}
+chordBtn.addEventListener('click', printChord)
+printChord()
 
-// USING ASYNC/AWAIT
-async function generateChord() {
+function generateChords(params) {
+  letters = params.letters
+  accidentals = params.accidentals
+  qualities = params.qualities
+  bassNotes = params.bassNotes
 
-  function random(array) {
+  let array = []
+  let chord = ''
+  for (i in letters) {
+    for (j in accidentals) {
+      for (k in qualities) {
+        for (l in bassNotes) {
+          chord = letters[i] + accidentals[j] + qualities[k] + bassNotes[l]
+          array.push(chord)
+        }
+      }
+    }
+  }
+
+  return array
+}
+
+
+// functions
+function random(array) {
     let i = Math.floor(Math.random() * array.length);
     let result = array[i];
     return result;
   }
 
-  function basicSeventhChords() {
-    const letters = [ 'C','D','E','F','G','A','B']
-    const accidentals = ['','♭','♯']
-    const qualities = ['Δ7','7','m7','ø']
-    const bassNotes = ['','III','V','VII']
-
-    let letter = random(letters);
-    let accidental = random(accidentals);
-    let quality = random(qualities);
-    let bassNote = random(bassNotes);
-    
-
-    let chordName = "chordName"
-    if (bassNote != '') {
-      chordName = letter + accidental + quality + "/" + bassNote
-    } else {
-      chordName = letter + accidental + quality 
-    }
-
-
-    console.log(chordName)
-    return chordName;
-
-    }
-
-  result = basicSeventhChords();
-
-  chordEl.innerHTML = result;
+function print(string) {
+  chordEl.innerHTML = string
 }
 

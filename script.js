3a4d3c4cfda986ccb950f662661d5f2b8728074d
Yi Fong Au <yifongau@gmail.com>
2024-12-25 13:30:24 +0100

    constructor (which takes logic)

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const chordEl = document.getElementById('chord')
 const chordBtn = document.getElementById('chordBtn')
+
 const chrdParams = new Object(); // possible chord parameters
     chrdParams.letters = ['C','D','E','F','G','A','B']
     chrdParams.accidentals = ['','♭','♯']
     chrdParams.qualities = ['Δ7','7','m7','ø']
-    chrdParams.bassNotes = ['','/III','/V','/VII']
+    chrdParams.bassNotes = ['','III','V','VII']
 
-let chordCache = generateChords(chrdParams) // possible chords
-const printChord = function() {  
-  print(random(chordCache))
-}
-chordBtn.addEventListener('click', printChord)
+let chordCache = generateChords(chrdParams, chordConstructor) // possible chords
+const printChord = function() { print(random(chordCache)) }
 printChord()
+chordBtn.addEventListener('click', printChord)
 
-function generateChords(params) {
+function chordConstructor(letter, accidental, quality, bassNote) {
+  chord = letter + accidental + quality + bassNote
+  return chord
+  console.log(chord)
+}
+
+function generateChords(params, constructor) {
   letters = params.letters
   accidentals = params.accidentals
   qualities = params.qualities
@@ -25,7 +30,8 @@ function generateChords(params) {
     for (j in accidentals) {
       for (k in qualities) {
         for (l in bassNotes) {
-          chord = letters[i] + accidentals[j] + qualities[k] + bassNotes[l]
+          chord = constructor(letters[i], accidentals[j], qualities[k], bassNotes[l])
+          constructor(letters[i], accidentals[j], qualities[k], bassNotes[l])
           array.push(chord)
         }
       }

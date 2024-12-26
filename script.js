207593865269469c3859aeb47f39a8301825c322
Yi Fong Au <yifongau@gmail.com>
2024-12-26 02:32:30 +0100

    implemented exclusions

--- script.js
+++ script.js
@@ -1,53 +1,85 @@
+// variables
 const chordEl = document.getElementById('chord')
 const chordBtn = document.getElementById('chordBtn')
 
-const chrdParams = new Object(); // possible chord parameters
-    chrdParams.letters = ['C','D','E','F','G','A','B']
-    chrdParams.accidentals = ['','♭','♯']
-    chrdParams.qualities = ['Δ7','7','m7','ø']
-    chrdParams.bassNotes = ['','III','V','VII']
+const chordParams = new Object(); // possible chord parameters
+chordParams.letters = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+chordParams.accidentals = ['', '♭', '♯']
+chordParams.qualities = ['Δ7', '7', 'm7', 'ø']
+chordParams.bassNotes = ['', 'III', 'V', 'VII']
+chordParams.exclusions = { rareEnharmonics: ['B♯', 'E♯', 'C♭', 'F♭'] }
 
-let chordCache = generateChords(chrdParams, chordConstructor) // possible chords
-const printChord = function() { print(random(chordCache)) }
+let C = 0
+let D = 0
+let E = 0
+let F = 0
+let G = 0
+let A = 0
+let B = 0
+
+
+// initialize app
+let chordCache = generateChords(chordParams, chordConstructor, chordFilter)
+const printChord = function () { chordElOutput(random(chordCache)) }
 printChord()
 chordBtn.addEventListener('click', printChord)
+
+// functions
+function chordElOutput(string) {
+  chordEl.innerHTML = string
+}
+
+
+function random(array) {
+  item = array[Math.floor(Math.random() * array.length)]
+  return item
+}
+
 
 function chordConstructor(letter, accidental, quality, bassNote) {
   chord = letter + accidental + quality + bassNote
   return chord
 }
 
-function generateChords(params, constructor) {
+function chordFilter(chord, exclusions) {
+  for (let exclusion in exclusions) {
+    substrings = exclusions[exclusion]
+    for (i in substrings) {
+      substring = substrings[i]
+      if (chord.includes(substring)) {
+        return 
+      }
+    }
+  }
+return chord
+
+}
+
+function generateChords(params, constructor, filter) {
   letters = params.letters
   accidentals = params.accidentals
   qualities = params.qualities
   bassNotes = params.bassNotes
+  exclusions = params.exclusions
 
   let array = []
   let chord = ''
   for (i in letters) {
+    let letter = letters[i]
     for (j in accidentals) {
+      let accidental = accidentals[j]
       for (k in qualities) {
+        let quality = qualities[k]
         for (l in bassNotes) {
-          chord = constructor(letters[i], accidentals[j], qualities[k], bassNotes[l])
-          array.push(chord)
+          let bassNote = bassNotes[l]
+          chord = filter(constructor(letter, accidental, quality, bassNote), exclusions)
+          if (chord) { array.push(chord) }
         }
       }
     }
   }
-
+  console.log(array)
   return array
 }
 
-
-// functions
-function random(array) {
-    let i = Math.floor(Math.random() * array.length);
-    let result = array[i];
-    return result;
-  }
-
-function print(string) {
-  chordEl.innerHTML = string
-}
 
